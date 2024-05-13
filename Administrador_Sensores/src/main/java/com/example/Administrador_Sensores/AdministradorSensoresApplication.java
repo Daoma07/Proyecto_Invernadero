f784317bbcd5d9436c37a02781fe5df77c9c9db0
Daniel Alameda <daoma222@gmail.com>
2024-05-13 12:27:43 -0700

    Se implmenta proeycto de sensores

---
+++ Administrador_Sensores/src/main/java/com/example/Administrador_Sensores/AdministradorSensoresApplication.java
@@ -0,0 +1,13 @@
+package com.example.Administrador_Sensores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministradorSensoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministradorSensoresApplication.class, args);
+	}
+
+}
